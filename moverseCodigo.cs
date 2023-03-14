515f4a66d09bba1159f82d57216e7f0511bf79aa
nicolas61v <67482852+nicolas61v@users.noreply.github.com>
2023-03-14 17:22:16 -0600

    codigo de movimiento

---
+++ moverseCodigo.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moverseCodigo : MonoBehaviour
+{
+    //el personaje se mueve hacia ambos lados
+
+    public float speed = 5f;
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+    }
+
+
+}
