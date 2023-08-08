115d0c97f0ac7c2c1406810c7a256016303efc88
nicolas61v <67482852+nicolas61v@users.noreply.github.com>
2023-08-08 16:44:22 -0500

    enemigo subido
    enemigo que se mueve y hace daño
    
    solo cambienar el nombre "IGUANA" por el nombre de tu personaje, puede ajustar la velocidad del enemigo y la velocidad hacia cada lado

--- enemy.cs
+++ enemy.cs
@@ -5,13 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class enemy : MonoBehaviour
 {
-    //death by enemy
+    public float speed = 3f;
+    public float timeLimit = 1f; 
+    private float timer = 0f; 
+    private int direction = 1;
+
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "iguana")
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 
+    void Update()
+    {
+        timer += Time.deltaTime; 
+        if (timer >= timeLimit) 
+        {
+            ChangeDirection();
+            timer = 0f; 
+        }
+        transform.Translate(Vector2.right * direction * speed * Time.deltaTime); 
+    }
+
+    void ChangeDirection()
+    {
+        direction *= -1; 
+    }
 }
